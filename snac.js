5e53e049edc173133e71fe3aebb59cd8330239f0
unknown <natnelldeisgn@gmail.com>
2020-05-09 21:23:25 +0800

    price check

--- snac.js
+++ snac.js
@@ -82,6 +82,7 @@ function gotResult(error, results,) {
     // console.log(results[0]);
      if (results[0].confidence>.9) {
      label = (results[0].label);
+     price = (price + 1);
      }
      else{
      label = "........";
