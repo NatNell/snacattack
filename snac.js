70a5d8360ce7ae7b0b63f3e6a53eac0ea4d9fa73
unknown <natnelldeisgn@gmail.com>
2020-05-09 20:26:38 +0800

    error

--- snac.js
+++ snac.js
@@ -82,7 +82,6 @@ function gotResult(error, results,) {
     // console.log(results[0]);
     if(results[0].confidence>90){
     label = results[0].label;
-    price = (price + 1);
     }
     else{
     label = "........";
